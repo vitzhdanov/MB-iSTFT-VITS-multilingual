6644cdca5c324d22f39bbad0f8a854869c442271
Misaki Tobisawa <92773327+misakiudon@users.noreply.github.com>
2022-11-20 12:02:24 +0900

    Update convert_to_22050.py

--- convert_to_22050.py
+++ convert_to_22050.py
@@ -5,14 +5,14 @@ import soundfile as sf
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument("--in_path", default="./tsukuyomi_raw", required=True)
-  parser.add_argument("--out_path", default="./tsukuyomi" ,required=True)
+  parser.add_argument("--in_path", default="./tsukuyomi_raw/", required=True)
+  parser.add_argument("--out_path", default="./tsukuyomi/" ,required=True)
 
   args = parser.parse_args()
 
-  os.makedirs(out_path, exist_ok=True)
-  filenames = os.listdir(in_path)
+  os.makedirs(args.out_path, exist_ok=True)
+  filenames = os.listdir(args.in_path)
   for filename in filenames:
-      print(in_path+filename)
-      y, sr = librosa.core.load(in_path+filename, sr=22050, mono=True)
-      sf.write(out_path+filename, y, sr, subtype="PCM_16")
+      print(args.in_path+filename)
+      y, sr = librosa.core.load(args.in_path+filename, sr=22050, mono=True)
+      sf.write(args.out_path+filename, y, sr, subtype="PCM_16")
